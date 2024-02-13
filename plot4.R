29d0c428adf24faeb988ebb71bbf2f62da8e51fd
imsohanb29 <159831072+imsohanb29@users.noreply.github.com>
2024-02-14 00:08:02 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,20 +1,7 @@
 plot4 <- function() {
-        ## Aim of this function is to 
-        ## 1. read the household_power_consumption.txt file
-        ## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
-        ## 3. generate 4 plots in 1 space (GAP vs. time, Vol vs. time, submetering vs. time and GRP vs. time)
-        
-        ## Parameters: none
-        ## Assumes household_power_consumption.txt file located in working dir
-        
-        ## read data
         powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
-        
-        ## Create column in table with date and time merged together
         FullTimeDate <- strptime(paste(powerdata$Date, powerdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
         powerdata <- cbind(powerdata, FullTimeDate)
-        
-        ## change class of all columns to correct class
         powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
         powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
         powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
@@ -24,11 +11,7 @@ plot4 <- function() {
         powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
         powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
         powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
-        
-        ## subset data from 2007-02-01 and 2007-02-02
         subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
-        
-        ## plot the 4 graphs
         png("plot4.png", width=480, height=480)
         par(mfrow=c(2,2))
         with(subsetdata, plot(FullTimeDate, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
